a55574b5bc0e16a28bd38b499c00f10b5f94eb1d
Ash Dev <ashdevbiz@gmail.com>
2026-02-01 11:10:35 +0530

    show controls ui

--- Assets/C# Script/showControls.cs
+++ Assets/C# Script/showControls.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class showControls : MonoBehaviour
 {
+    [Header("UI References")]
+    public Image controlsImage;
+    public Button toggleButton;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Add listener to button
+        if (toggleButton != null)
+        {
+            toggleButton.onClick.AddListener(ToggleImage);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Toggle the image on/off
+    void ToggleImage()
     {
-        
+        if (controlsImage != null)
+        {
+            controlsImage.enabled = !controlsImage.enabled;
+        }
+    }
+
+    void OnDestroy()
+    {
+        // Clean up listener when object is destroyed
+        if (toggleButton != null)
+        {
+            toggleButton.onClick.RemoveListener(ToggleImage);
+        }
     }
 }

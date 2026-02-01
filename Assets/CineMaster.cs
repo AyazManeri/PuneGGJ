d5a1f5370098b4ee2a711849cf352ee596333faf
AyazManeri <ayazmaneri1127@gmail.com>
2026-02-01 15:03:58 +0530

    little change

--- Assets/CineMaster.cs
+++ Assets/CineMaster.cs
@@ -1,16 +1,86 @@
 using UnityEngine;
+using Unity.Cinemachine;
 
 public class CineMaster : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Cinemachine Camera")]
+    public CinemachineCamera cinemachineCamera;
+
     void Start()
     {
-        
+        if (cinemachineCamera == null)
+        {
+            cinemachineCamera = GetComponent<CinemachineCamera>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetLensSize(float size)
     {
-        
+        if (cinemachineCamera != null)
+        {
+            cinemachineCamera.Lens.OrthographicSize = size;
+        }
+    }
+
+    public void SetFieldOfView(float fov)
+    {
+        if (cinemachineCamera != null)
+        {
+            cinemachineCamera.Lens.FieldOfView = fov;
+        }
+    }
+
+    public void SetNearClipPlane(float nearClip)
+    {
+        if (cinemachineCamera != null)
+        {
+            cinemachineCamera.Lens.NearClipPlane = nearClip;
+        }
+    }
+
+    public void SetFarClipPlane(float farClip)
+    {
+        if (cinemachineCamera != null)
+        {
+            cinemachineCamera.Lens.FarClipPlane = farClip;
+        }
+    }
+
+    public void SetLensSettings(float fov, float nearClip, float farClip)
+    {
+        if (cinemachineCamera != null)
+        {
+            cinemachineCamera.Lens.FieldOfView = fov;
+            cinemachineCamera.Lens.NearClipPlane = nearClip;
+            cinemachineCamera.Lens.FarClipPlane = farClip;
+        }
+    }
+
+    public void SetOrthographicSettings(float size, float nearClip, float farClip)
+    {
+        if (cinemachineCamera != null)
+        {
+            cinemachineCamera.Lens.OrthographicSize = size;
+            cinemachineCamera.Lens.NearClipPlane = nearClip;
+            cinemachineCamera.Lens.FarClipPlane = farClip;
+        }
+    }
+
+    public float GetLensSize()
+    {
+        if (cinemachineCamera != null)
+        {
+            return cinemachineCamera.Lens.OrthographicSize;
+        }
+        return 0f;
+    }
+
+    public float GetFieldOfView()
+    {
+        if (cinemachineCamera != null)
+        {
+            return cinemachineCamera.Lens.FieldOfView;
+        }
+        return 0f;
     }
 }
